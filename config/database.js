8c71d5753bf778e0aacc8e8bb0e8deb115205b0c
paulwangdalijr <paulwangdalijr@gmail.com>
2018-05-15 19:48:34 +0800

    update 5

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports =
 {
-    uri:'mongodb://localhost:27017/mean-angular2',
-    // uri:'mongodb://root:root@ds123490.mlab.com:23490/mean-angular2',
+    // uri:'mongodb://localhost:27017/mean-angular2',
+    uri:'mongodb://root:root@ds123490.mlab.com:23490/mean-angular2',
     secret: crypto,
     db: 'mean-angular2'
 }
